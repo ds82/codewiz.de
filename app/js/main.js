4d32c83ad39460472ff2da9e11f15e2e03213cd5
Dennis Sänger <mail@dennis.io>
2015-08-14 21:10:04 +0200

    update footer information

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,15 @@
 window.jQuery = window.$ = require('thirdparty/jquery');
 require('thirdparty/bootstrap');
-
-//require('thirdparty/freelancer/js/custom.js');
-
 require('thirdparty/angular/angular.js');
 
 var app = angular.module('app', []);
 
-app.controller('AppController', ['$scope', function( $scope ) {
-
+app.controller('AppController', ['$scope', function($scope) {
 
 }]);
+
+app.controller('FooterCtrl', FooterCtrl);
+function FooterCtrl() {
+  var vm = this;
+  vm.today = new Date();
+}
